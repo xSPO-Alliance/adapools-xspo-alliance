c2e1e8f1b72d03ca43dee988ff92ee9b3ccdc48c
Rodolfo Neto <rodolfotiago@gmail.com>
2022-12-16 17:05:38 +0000

    REVERT FROM HERE: chore: tests with verification code

---
+++ scripts/discord-bot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import discord
+from discord.ext import commands, tasks
+import random
+import sys
+
+_user = sys.argv[1]
+
+ver_code = ''.join((random.choice('xSPOAdisc') for i in range(9)))
+print(ver_code)
+
+bot = commands.Bot(
+    intents=discord.Intents.all() ,
+    command_prefix='>' ,
+    description='Registration verification code'
+    )
+
+@bot.command()
+async def code(ctx):
+    
+    ver_author=str(ctx.author)
+    if ver_author == _user:
+        try:
+            ver_code_from_author=str(ctx.message.content).split(" ")[1]
+            if ver_code == ver_code_from_author:
+                await ctx.send(ver_author)
+                await ctx.send("Code verified successfully")
+                exit()
+            else:
+                await ctx.send(ver_author)
+                await ctx.send("Wrong code: " + ver_code_from_author)
+                exit(-1)
+
+        except Exception:
+            exit(-1)
+
+bot.run('NjAwMzY1OTcxNjcyMTM3NzQ4.GkdDm5.eUh_kj4lrTqUkOXJ40marS4-I2UgRIo8OZuH8c')
